6080626827da47291f18206a23c7962cee04ca07
Kevin S. Clarke <ksclarke@ksclarke.io>
2021-11-03 00:24:46 -0400

    Update versions and add tests

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # maven-graalvm-musl
 
-This project builds a Docker image that's intended to be used as the source for the first phase of a multistage build.
+## Introduction
 
-### Contact Information
+A Docker image that contains [Maven](https://maven.apache.org/), [GraalVM](https://www.graalvm.org/) (including its [native-image](https://www.graalvm.org/reference-manual/native-image/) program), and the [musl libc](https://musl.libc.org/) library. It's intended to be used as the first stage in a multistage Docker build that produces an Alpine image.
 
-If you notice something that is broken or that needs fixing, please submit a ticket to the project's [issues queue](https://github.com/ksclarke/maven-graalvm-musl/issues). If you have a question or a general comment about the project, please use the project's [discussion board](https://github.com/ksclarke/maven-graalvm-musl/discussions).
+## For Additional Information
 
-Thanks for your interest in this project!
+The source code for this image can be found on the project's [GitHub page](https://github.com/ksclarke/maven-graalvm-musl).
+
+If you notice something broken, please submit a ticket to the project's [issues queue](https://github.com/ksclarke/maven-graalvm-musl/issues). If you have a question or a general comment about the project, please use the project's [discussion board](https://github.com/ksclarke/maven-graalvm-musl/discussions).
+
+Thanks for your interest!
---
+++ src/test/java/info/freelibrary/maven/graalvm/MgmImageFT.java
@@ -0,0 +1,233 @@
+
+package info.freelibrary.maven.graalvm;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.testcontainers.containers.Container.ExecResult;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+/**
+ * Tests of our newly built container image.
+ */
+public class MgmImageFT {
+
+    /**
+     * The name of the image we want to test.
+     */
+    public static final DockerImageName DOCKER_IMAGE_NAME = DockerImageName.parse("maven-graalvm-musl:latest");
+
+    /**
+     * An instance of the container that we can run tests against. That container doesn't have a running process so we
+     * add one to keep the container up while testing.
+     */
+    @ClassRule
+    public static final GenericContainer<?> MGM_CONTAINER = new GenericContainer<>(DOCKER_IMAGE_NAME);
+
+    /**
+     * The expected MUSL GCC executable.
+     */
+    private static final String MUSL_GCC = "musl-gcc";
+
+    /**
+     * The expected location of the Maven directory.
+     */
+    private static final String MAVEN_HOME = "/opt/maven";
+
+    /**
+     * Tests the Maven executable.
+     *
+     * @throws InterruptedException If the container test is interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    @Test
+    public final void testMavenExec() throws InterruptedException, IOException {
+        final ExecResult result = which("mvn");
+
+        assertEquals(0, result.getExitCode());
+        assertEquals("/opt/maven/bin/mvn", result.getStdout().trim());
+    }
+
+    /**
+     * Tests the UPX executable.
+     *
+     * @throws InterruptedException If the container test is interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    @Test
+    public final void testUpxExec() throws InterruptedException, IOException {
+        final ExecResult result = which("upx");
+
+        assertEquals(0, result.getExitCode());
+        assertEquals("/mgm_tools/bin/upx", result.getStdout().trim());
+    }
+
+    /**
+     * Tests the musl-gcc executable.
+     *
+     * @throws InterruptedException If the container test is interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    @Test
+    public final void testMuslGccExec() throws InterruptedException, IOException {
+        final ExecResult result = which(MUSL_GCC);
+
+        assertEquals(0, result.getExitCode());
+        assertEquals("/mgm_tools/bin/musl-gcc", result.getStdout().trim());
+    }
+
+    /**
+     * Tests the native-image executable.
+     *
+     * @throws InterruptedException If the container test is interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    @Test
+    public final void testNativeImageExec() throws InterruptedException, IOException {
+        final ExecResult result = which("native-image");
+
+        assertEquals(0, result.getExitCode());
+        assertEquals("/usr/bin/native-image", result.getStdout().trim());
+    }
+
+    /**
+     * Tests the XZ executable.
+     *
+     * @throws InterruptedException If the container test is interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    @Test
+    public final void testXzExec() throws InterruptedException, IOException {
+        final ExecResult result = which("xz");
+
+        assertEquals(0, result.getExitCode());
+        assertEquals("/usr/bin/xz", result.getStdout().trim());
+    }
+
+    /**
+     * Tests the presence of the cacerts.
+     *
+     * @throws InterruptedException If the container test is interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    @Test
+    public final void testCacertsPresence() throws InterruptedException, IOException {
+        assertTrue(fileExists("/etc/default/cacerts"));
+    }
+
+    /**
+     * Tests the presence of libstdc++.
+     *
+     * @throws InterruptedException If the container test is interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    @Test
+    public final void testLibstcdPresence() throws InterruptedException, IOException {
+        assertTrue(fileExists("/mgm_tools/lib/libstdc++.a"));
+    }
+
+    /**
+     * Tests the presence of ZLib.
+     *
+     * @throws InterruptedException If the container test is interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    @Test
+    public final void testZlibPresence() throws InterruptedException, IOException {
+        assertTrue(fileExists("/mgm_tools/lib/libz.a"));
+    }
+
+    /**
+     * Tests the presence of the README.
+     *
+     * @throws InterruptedException If the container test is interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    @Test
+    public final void testReadmePresence() throws InterruptedException, IOException {
+        assertTrue(fileExists("/README.md"));
+    }
+
+    /**
+     * Tests the presence of the MAVEN_HOME ENV.
+     *
+     * @throws InterruptedException If the container test is interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    @Test
+    public final void testMavenHomeENV() throws InterruptedException, IOException {
+        final ExecResult result = echo("MAVEN_HOME");
+
+        assertEquals(0, result.getExitCode());
+        assertEquals(MAVEN_HOME, result.getStdout().trim());
+    }
+
+    /**
+     * Tests the presence of the M2_HOME ENV.
+     *
+     * @throws InterruptedException If the container test is interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    @Test
+    public final void testM2HomeENV() throws InterruptedException, IOException {
+        final ExecResult result = echo("M2_HOME");
+
+        assertEquals(0, result.getExitCode());
+        assertEquals(MAVEN_HOME, result.getStdout().trim());
+    }
+
+    /**
+     * Tests the presence of the CC ENV.
+     *
+     * @throws InterruptedException If the container test is interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    @Test
+    public final void testCcENV() throws InterruptedException, IOException {
+        final ExecResult result = echo("CC");
+
+        assertEquals(0, result.getExitCode());
+        assertEquals(MUSL_GCC, result.getStdout().trim());
+    }
+
+    /**
+     * Runs a which command inside the test container.
+     *
+     * @param aProgram A program to find using the which command
+     * @return The result of the which
+     * @throws InterruptedException If interactions with the container are interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    private ExecResult which(final String aProgram) throws InterruptedException, IOException {
+        return MGM_CONTAINER.execInContainer("which", aProgram);
+    }
+
+    /**
+     * Runs an echo command inside the test container to get the value of the supplied ENV property.
+     *
+     * @param aEnvProperty An environmental property
+     * @return The result of the which
+     * @throws InterruptedException If interactions with the container are interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    private ExecResult echo(final String aEnvProperty) throws InterruptedException, IOException {
+        return MGM_CONTAINER.execInContainer("bash", "-c", "echo $" + aEnvProperty);
+    }
+
+    /**
+     * Tests whether the supplied file path exists within the container.
+     *
+     * @param aFilePath A path to a file
+     * @return True if the file exists; else, false
+     * @throws InterruptedException If interactions with the container are interrupted
+     * @throws IOException If there is trouble reading from the container
+     */
+    private boolean fileExists(final String aFilePath) throws InterruptedException, IOException {
+        return MGM_CONTAINER.execInContainer("ls", aFilePath).getExitCode() == 0;
+    }
+}
